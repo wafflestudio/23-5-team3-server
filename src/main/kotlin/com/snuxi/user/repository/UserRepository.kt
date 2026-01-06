d83a7a83ce2669cf251e930ac5dec18fcf7e42ea
mined2022 <mined2078@gmail.com>
2026-01-06 22:58:21 +0900

    refactor: Rename function and parameter

--- src/main/kotlin/com/snuxi/user/repository/UserRepository.kt
+++ src/main/kotlin/com/snuxi/user/repository/UserRepository.kt
@@ -4,5 +4,5 @@ import com.snuxi.user.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository: JpaRepository<User, Long> {
-    fun findByUserEmail(userEmail: String): User?
+    fun findByEmail(email: String): User?
 }
