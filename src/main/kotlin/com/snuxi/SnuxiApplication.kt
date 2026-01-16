6169a5bfd43fe84797216d68cd9e41145027ff39
Ubuntu <ubuntu@ip-172-31-45-2.ap-northeast-2.compute.internal>
2026-01-16 11:44:49 +0000

    hotfix: login problem

--- src/main/kotlin/com/snuxi/SnuxiApplication.kt
+++ src/main/kotlin/com/snuxi/SnuxiApplication.kt
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration
 import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration
 
 //exclude = 테스트 용. 개발 후 삭제 필요
-@SpringBootApplication()
+@SpringBootApplication(exclude = [
+        org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration::class
+    ])
 class SnuxiApplication
 
 fun main(args: Array<String>) {
