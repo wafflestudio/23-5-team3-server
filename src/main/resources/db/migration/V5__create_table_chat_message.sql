28fe1f387ea67b7cf8fe3a03b079fa480a0d016a
SugAr <kkwandy@naver.com>
2026-01-18 22:04:31 +0900

    add V5_chat DB

---
+++ src/main/resources/db/migration/V5__create_table_chat_message.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS chat_message
+(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    pot_id BIGINT,
+    sender_id BIGINT,
+    text TEXT,
+    datetime_send_at TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT __chat_message_fk_pot_id FOREIGN KEY (pot_id) REFERENCES pots (id) ON DELETE CASCADE,
+    CONSTRAINT __chat_message_fk_sender_id FOREIGN KEY (sender_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX __chat_message_idx_pot_id_datetime_send_at ON chat_message (pot_id, datetime_send_at)
