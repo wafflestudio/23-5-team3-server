3701be51a0c03f55b8b0eb3c7f598e1cd980f7ee
SugAr <kkwandy@naver.com>
2026-01-30 13:38:42 +0900

    add cors patch

--- src/main/kotlin/com/snuxi/config/SecurityConfig.kt
+++ src/main/kotlin/com/snuxi/config/SecurityConfig.kt
@@ -68,7 +68,7 @@ class SecurityConfig(
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
         configuration.allowedOrigins = listOf("http://localhost:5173", "https://d2c0wdnl0iqvgb.cloudfront.net", "https://snuxi.com")
-        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
 
