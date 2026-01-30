02a9fa02efe02e47b9af0ae088cf7e6bc2b9ff7b
mined2022 <mined2078@gmail.com>
2026-01-30 17:13:16 +0900

    add: allowedOrigins에 https://d2j21bk78krg0p.cloudfront.net 추가

--- src/main/kotlin/com/snuxi/config/SecurityConfig.kt
+++ src/main/kotlin/com/snuxi/config/SecurityConfig.kt
@@ -79,7 +79,7 @@ class SecurityConfig(
     @Bean
     fun corsConfigurationSource(): CorsConfigurationSource {
         val configuration = CorsConfiguration()
-        configuration.allowedOrigins = listOf("http://localhost:5173", "https://d2c0wdnl0iqvgb.cloudfront.net", "https://snuxi.com")
+        configuration.allowedOrigins = listOf("http://localhost:5173", "https://d2c0wdnl0iqvgb.cloudfront.net", "https://snuxi.com", "https://d2j21bk78krg0p.cloudfront.net")
         configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
         configuration.allowedHeaders = listOf("*")
         configuration.allowCredentials = true
