bd78975ee743c2ea9a7f82af50b2bc75a2031987
mined2022 <mined2078@gmail.com>
2026-02-03 20:46:43 +0900

    feat: 정지된 유저 접근 시 발생시킬 예외 추가

--- src/main/kotlin/com/snuxi/user/UserException.kt
+++ src/main/kotlin/com/snuxi/user/UserException.kt
@@ -3,6 +3,8 @@ package com.snuxi.user
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatusCode
 import com.snuxi.exception.DomainException
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import org.springframework.security.oauth2.core.OAuth2Error
 
 sealed class UserException(
     errorCode: Int,
@@ -27,5 +29,12 @@ class UserNotFoundException : UserException(
     errorCode = 0,
     httpStatusCode = HttpStatus.NOT_FOUND,
     msg = "해당 유저를 찾을 수 없습니다."
+)
+
+class SuspendedUserException(
+    msg: String
+) : OAuth2AuthenticationException(
+    OAuth2Error("suspended_user"),
+    msg
 )
 
